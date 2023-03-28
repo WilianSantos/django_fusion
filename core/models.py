7e0c15f78703f6020a481fa808d5a0f4723f2bee
Wilian Soares dos Santos <wilian.santos3@outlook.com>
2023-03-28 12:06:46 -0300

    1° dia completo

---
+++ core/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+from stdimage.models import StdImageField
+
+class Base(models.Model):
+    criados = models.DateField('Criação', auto_now_add=True)
+    modificado = models.DateField('Atualização', auto_now=True)
+    ativo = models.BooleanField('Ativo', default=True)
+    
+    
+    class Meta:
+        abstract = True
+        
+        
+class Servico(Base):
+    ICONE_CHOICES = (
+        ('lni-cog', 'Engrenagem'),
+        ('lni-status-up', 'Gráfico'),
+        ('lni-users', 'Usuários'),
+        ('lni-layers', 'Design'),
+        ('lni-mobile', 'Mobile'),
+        ('lni-rocket', 'Foguete'),
+    )
+    servico = models.CharField('serviço', max_length=100)
+    descricao = models.TextField('Descrição', max_length=200)
+    icone = models.CharField('Icone', max_length=13, choices=ICONE_CHOICES)
+    
+    
+    class Meta:
+        verbose_name = 'Serviço'
+        verbose_name_plural = 'Serviços'
+        
+        def __str__(self):
+            return self.Servico
